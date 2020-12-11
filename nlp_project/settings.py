0d3120363b531575fff796f03bfb977e95e2cbb3
Narendra186 <40228592+Narendra186@users.noreply.github.com>
2020-12-11 21:03:28 +0530

    modified allowed hosts

--- nlp_project/settings.py
+++ nlp_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'j*shp#o)jsl$!9))b0q(xv+k$pzz28+nx-g*gb*%hsh_hc!@mm'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','sampledomain.com']
+ALLOWED_HOSTS = ['127.0.0.1','duplicate-questions-detection.herokuapp.com']
 
 
 # Application definition
