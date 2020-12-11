ab7189e33f398ed754e48798cd796d662b0ef24a
Narendra186 <40228592+Narendra186@users.noreply.github.com>
2020-12-11 21:17:56 +0530

    changed static root

--- nlp_project/settings.py
+++ nlp_project/settings.py
@@ -125,5 +125,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, '/')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
